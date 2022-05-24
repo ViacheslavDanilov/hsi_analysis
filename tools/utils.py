08500a8523c23444b9c0f248a8a4a3107c351589
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2022-05-24 19:39:09 +0200

    [Feature]: add function returning a list of files with specific extensions

--- tools/utils.py
+++ tools/utils.py
@@ -1,25 +1,27 @@
 import os
+from pathlib import Path
+from typing import List, Union
 
 import numpy as np
 from struct import unpack
 
 
-def read_dat(
+def read_hsi(
         path: str,
 ) -> np.ndarray:
-    """ Read .dat files as a numpy array (H, W, WL).
-
+    """ Read HSI images (.dat files) as a numpy array (H, W, WL).
     Args:
         path: a path to a .dat file being read
     """
 
-    if (
-            isinstance(path, str)
-            and os.path.isfile(path)
-    ):
-        pass
+    if not isinstance(path, str):
+        raise ValueError(f'Invalid data type: {type(path)}')
+    elif not os.path.isfile(path):
+        raise ValueError(f"The file doesn't exist: {path}")
+    elif not path.lower().endswith('.dat'):
+        raise ValueError(f"The file with extension {Path(path).suffix} is not supported at the moment")
     else:
-        raise ValueError(f"The file doesn't exist or is not readable: {path}")
+        pass
 
     with open(path, 'rb') as fp:
         header = fp.read(3 * 4)
@@ -32,8 +34,35 @@ def read_dat(
         return data
 
 
-if __name__ == '__main__':
+def get_file_list(
+    src_dirs: Union[List[str], str],
+    ext_list: Union[List[str], str],
+    include_template: str = '',
+) -> List[str]:
+    """
+    Get list of files with the specified extensions
+    Args:
+        src_dirs: directory(s) with files inside
+        ext_list: extension(s) used for a search
+        include_template: include directories with this template
+    Returns:
+        all_files: a list of file paths
+    """
 
-    data_path = 'dataset/Test 1 - Liver LA contactless switching off laser/HSI/2019_08_30_10_00_39/2019_08_30_10_00_39_SpecCube.dat'
-    data = read_dat(data_path)
-    print(np.mean(data), np.std(data), np.min(data), np.max(data))
+    all_files = []
+    src_dirs = [src_dirs, ] if isinstance(src_dirs, str) else src_dirs
+    ext_list = [ext_list, ] if isinstance(ext_list, str) else ext_list
+    for src_dir in src_dirs:
+        for root, dirs, files in os.walk(src_dir):
+            for file in files:
+                file_ext = Path(file).suffix
+                file_ext = file_ext.lower()
+                dir_name = os.path.basename(root)
+                if (
+                    file_ext in ext_list
+                    and include_template in dir_name
+                ):
+                    file_path = os.path.join(root, file)
+                    all_files.append(file_path)
+    all_files.sort()
+    return all_files
