fe4610603d3c032f05dd5a32e12ee6fe546ea9bb
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2022-06-01 14:31:43 +0200

    [Feature]: implement selection of data types and color maps

--- tools/convert_hsi.py
+++ tools/convert_hsi.py
@@ -13,7 +13,7 @@ import pandas as pd
 from tqdm import tqdm
 from tqdm.contrib.concurrent import process_map
 
-from tools.utils import get_file_list, read_hsi, extract_body_part, extract_time_stamp
+from tools.utils import get_file_list, read_hsi, extract_body_part, extract_time_stamp, get_color_map
 
 os.makedirs('logs', exist_ok=True)
 logging.basicConfig(
@@ -28,6 +28,8 @@ logger = logging.getLogger(__name__)
 
 def process_hsi(
         file_path: str,
+        data_type: str,
+        color_map: str,
         output_type: str,
         output_size: Tuple[int, int],
         fps: int,
@@ -35,7 +37,10 @@ def process_hsi(
 ) -> List:
 
     # Read HSI file and extract additional information
-    hsi = read_hsi(file_path)
+    hsi = read_hsi(
+        path=file_path,
+        data_type=data_type,
+    )
     body_part = extract_body_part(file_path)
     date, time = extract_time_stamp(filename=Path(file_path).name)
 
@@ -69,7 +74,7 @@ def process_hsi(
         metadata.append(
             {
                 'Source dir': str(Path(file_path).parent),
-                'HSI name': Path(file_path).name,
+                'HSI name': str(Path(file_path).name),
                 'Date': date,
                 'Time': time,
                 'Body part': body_part,
@@ -84,14 +89,21 @@ def process_hsi(
             }
         )
 
-        # Normalize and process image
-        image = image.astype(np.float32)
-        image = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-        image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+        # Resize and normalize image
         image_size = image.shape[:-1] if len(image.shape) == 3 else image.shape
         if image_size != output_size:
             image = imutils.resize(image, height=output_size[0], inter=cv2.INTER_LINEAR)
+        image = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
 
+        # Colorize image
+        if isinstance(color_map, str) and color_map is not None:
+            cmap = get_color_map(color_map)
+            image = cv2.applyColorMap(image, cmap)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        else:
+            image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+
+        # Save image
         if output_type == 'image':
             cv2.imwrite(image_path, image)
         elif output_type == 'video':
@@ -108,6 +120,8 @@ def process_hsi(
 
 def main(
         input_dir: str,
+        data_type: str,
+        color_map: str,
         output_type: str,
         output_size: Tuple[int, int],
         fps: int,
@@ -115,11 +129,13 @@ def main(
 ):
 
     # Log main parameters
-    logger.info(f"Input dir..........: {input_dir}")
-    logger.info(f"Output dir.........: {save_dir}")
-    logger.info(f"Output type........: {output_type}")
-    logger.info(f"Output size........: {output_size}")
-    logger.info(f"FPS................: {fps if output_type == 'video' else None}")
+    logger.info(f'Input dir..........: {input_dir}')
+    logger.info(f'Output dir.........: {save_dir}')
+    logger.info(f'Data type..........: {data_type}')
+    logger.info(f'Color map..........: {color_map}')
+    logger.info(f'Output type........: {output_type}')
+    logger.info(f'Output size........: {output_size}')
+    logger.info(f'FPS................: {fps if output_type == "video" else None}')
     logger.info('')
 
     # Get list of HSI files
@@ -135,6 +151,8 @@ def main(
     num_cores = multiprocessing.cpu_count()
     processing_func = partial(
         process_hsi,
+        data_type=data_type,
+        color_map=color_map,
         output_type=output_type,
         output_size=output_size,
         fps=fps,
@@ -147,7 +165,7 @@ def main(
     )
     metadata = sum(_metadata, [])
 
-    # Save metadata as an xlsx file
+    # Save metadata as an XLSX file
     df = pd.DataFrame(metadata)
     df.sort_values('Image path', inplace=True)
     save_path = os.path.join(save_dir, 'metadata.xlsx')
@@ -163,16 +181,20 @@ def main(
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Data processing')
+    parser = argparse.ArgumentParser(description='Convert Hyperspectral Images')
     parser.add_argument('--input_dir', default='dataset', type=str)
-    parser.add_argument('--output_type', default='image', type=str, help='image or video')
-    parser.add_argument('--output_size', nargs='+', default=[1000, 1000], type=int)
+    parser.add_argument('--data_type',  default='absorbance', type=str, choices=['absorbance', 'reflectance'])
+    parser.add_argument('--color_map', default=None, type=str, choices=['jet', 'bone', 'ocean', 'cool', 'hsv'])
+    parser.add_argument('--output_type', default='image', type=str, choices=['image', 'video'])
+    parser.add_argument('--output_size', default=[744, 1000], nargs='+', type=int)
     parser.add_argument('--fps', default=15, type=int)
-    parser.add_argument('--save_dir', default='experiments', type=str)
+    parser.add_argument('--save_dir', default='dataset/converted', type=str)
     args = parser.parse_args()
 
     main(
         input_dir=args.input_dir,
+        data_type=args.data_type,
+        color_map=args.color_map,
         output_type=args.output_type,
         output_size=tuple(args.output_size),
         fps=args.fps,
