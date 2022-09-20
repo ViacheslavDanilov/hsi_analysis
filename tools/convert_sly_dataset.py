f90d7ca5ee50e4342eb83af8ac5c00dbe138e1ee
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2022-09-20 23:12:58 +0300

    [WiP]: add multiprocessing parsing of video files

--- tools/convert_sly_dataset.py
+++ tools/convert_sly_dataset.py
@@ -1,12 +1,14 @@
 import json
 import argparse
+from functools import partial
+from joblib import Parallel, delayed
 
 import numpy as np
 from tqdm import tqdm
-from sklearn.utils import class_weight
-from sklearn.model_selection import train_test_split
 
+from tools.utils import crop_image
 from tools.supervisely_utils import *
+
 
 os.makedirs('logs', exist_ok=True)
 logging.basicConfig(
@@ -19,180 +21,107 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def crop(
-        input_img: np.ndarray,
-        img_type: str = 'absorbance',
-) -> np.ndarray:
+def process_single_video(
+        group: Tuple[int, pd.DataFrame],
+        img_type: str,
+        # dataset_dir: str,
+        # save_pairs_only: bool,
+        save_dir: str,
+) -> pd.DataFrame:
+    _, row = group
+    video_path = row['video_path']
+    stem = row['stem']
+    test = row['test']
 
-    assert input_img.shape[1] % 3 == 0, 'Input image width should be divisible by 3 i.e. contain 3 sub-images'
+    # Create image and annotation directories
+    img_dir = os.path.join(save_dir, test, stem, 'img')
+    ann_dir = os.path.join(save_dir, test, stem, 'ann')
+    os.makedirs(img_dir, exist_ok=True)
+    os.makedirs(ann_dir, exist_ok=True)
 
-    img_width = int(input_img.shape[1] / 3)
-    if img_type == 'absorbance':
-        idx = 0
-    elif img_type == 'hsv':
-        idx = 1
-    elif img_type == 'reflectance':
-        idx = 2
-    else:
-        raise ValueError(f'Invalid img_type: {img_type}')
+    # Read video, save images, and empty annotations
+    # TODO: VERSION 1
+    video = cv2.VideoCapture(video_path)
+    frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    count = 0
+    while True:
+        success, _img = video.read()
+        if not success:
+            break
+        img = crop_image(
+            input_img=_img,
+            img_type=img_type
+        )
+        img_path = os.path.join(img_dir, f'{count+1:03d}.png')
+        cv2.imwrite(img_path, img)
 
-    output_img = input_img[:, idx * img_width:(idx + 1) * img_width]
+        txt_path = os.path.join(ann_dir, f'{count+1:03d}.txt')
+        f = open(file=txt_path, mode='w')
+        f.close()
+        count += 1
 
-    return output_img
+    # Read json with metadata
+    ann_path = row['ann_path']
+    f = open(ann_path)
+    ann_data = json.load(f)
+    img_height = ann_data['size']['height']
+    img_width = ann_data['size']['width'] // 2          # delimiter is a number of images in a stacked row
 
+    # Extract object keys based on annotator name
+    # TODO: remove after debugging bboxes
+    key_obj_map = {}
+    for obj in ann_data['objects']:
+        key_obj_map[obj['classTitle']] = obj['key']
+
+    # Save annotations
+    for frame in ann_data['frames']:
+        frame_idx = frame['index']
+        # TODO: Iterate over figures
+        for _figure in frame['figures']:
+
+            figure = sly.Rectangle.from_json(_figure['geometry'])
+
+            if figure.left > img_width:
+                figure = figure.translate(drow=img_width, dcol=0)
+
+    return 1
 
 
 def main(
         df: pd.DataFrame,
-        class_names: Tuple[str],
-        exclude_empty_masks: bool,
-        use_smoothing: bool,
+        annotator: str,
+        box_extension: int,
         img_type: str,
         save_dir: str,
 ) -> None:
 
-    logger.info(f'Classes..............: {class_names}')
-    logger.info(f'Number of classes....: {len(class_names)}')
-    logger.info(f'Exclude empty masks..: {exclude_empty_masks}')
+    logger.info(f'Annotator............: {annotator}')
+    logger.info(f'Box extension........: {box_extension}')
     logger.info(f'Output directory.....: {save_dir}')
 
     tests = list(set(df['test']))
     logger.info(f'Number of tests......: {len(tests)}')
 
-    # FIXME: Split dataset into train, val and test subsets
-    # Iterate over tests
-    for test in tests:
-        df_test = df[df['test'] == test]
-
-        # # Iterate over videos
-        for idx, row in tqdm(df_test.iterrows(), desc='Data conversion', unit=' video'):
-            video_path = row['video_path']
-            stem = row['stem']
-
-            # Create image and annotation directories
-            img_dir = os.path.join(save_dir, test, stem, 'img')
-            os.makedirs(img_dir, exist_ok=True)
-
-            # Read video and save images
-            # TODO: uncomment later
-            # video = cv2.VideoCapture(video_path)
-            # count = 0
-            # while True:
-            #     success, _img = video.read()
-            #     if not success:
-            #         break
-            #     img = crop(
-            #         input_img=_img,
-            #         img_type=img_type
-            #     )
-            #     img_path = os.path.join(img_dir, f'image_{count:02d}.png')
-            #     cv2.imwrite(img_path, img)
-            #     count += 1
-
-            # TODO: Read json and save masks
-            ann_path = row['ann_path']
-            ann_dir = os.path.join(save_dir, test, stem, 'ann')
-            os.makedirs(ann_dir, exist_ok=True)
-
-            f = open(ann_path)
-            ann_data = json.load(f)
-            img_height, img_width = (
-                ann_data['size']['height'],
-                ann_data['size']['width'],
-            )
-
-    # TODO: Iterate over image-annotation pairs
-    data = np.array([], dtype=np.uint8)
-    for idx, row in tqdm(df_final.iterrows(), desc='Image processing', unit=' images'):
-        filename = row['filename']
-        img_path = row['img_path']
-        ann_path = row['ann_path']
-        f = open(ann_path)
-        ann_data = json.load(f)
-        img_size = (
-            ann_data['size']['height'],
-            ann_data['size']['width'],
-        )
-
-        # Iterate over objects
-        mask = np.zeros(img_size, dtype=np.uint8)
-        palette = get_palette(class_names)
-
-        for obj in ann_data['objects']:
-            class_name = obj['classTitle']
-            if class_name in class_names:
-                class_id = class_names.index(class_name)
-                obj_mask64 = obj['bitmap']['data']
-                obj_mask = base64_to_mask(obj_mask64)
-                if use_smoothing:
-                    obj_mask = smooth_mask(obj_mask)
-                obj_mask = obj_mask.astype(float)
-                obj_mask *= class_id/np.max(obj_mask)
-                obj_mask = obj_mask.astype(np.uint8)
-
-                mask = insert_mask(
-                    mask=mask,
-                    obj_mask=obj_mask,
-                    origin=obj['bitmap']['origin'],
-                )
-        logger.debug('Empty mask: {:s}'.format(Path(filename).name))
-
-        if (
-                np.sum(mask) == 0
-                and exclude_empty_masks
-        ):
-            pass
-        else:
-            subset = row['subset']
-
-            # Save image
-            img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-            img_save_path = os.path.join(save_dir, 'img_dir', subset, '{:s}.png'.format(filename))
-            cv2.imwrite(img_save_path, img)
-
-            # Accumulate masks for class weight calculation
-            mask_vector = np.concatenate(mask)
-            data = np.append(data, mask_vector, axis=0)
-
-            # Save colored mask
-            mask = Image.fromarray(mask).convert('P')
-            mask.putpalette(np.array(palette, dtype=np.uint8))
-            mask_save_path = os.path.join(save_dir, 'ann_dir', subset, '{:s}.png'.format(filename))
-            mask.save(mask_save_path)
-
-    # Compute class weights
-    class_weights = class_weight.compute_class_weight(
-        y=data,
-        classes=np.unique(data),
-        class_weight='balanced',
+    processing_func = partial(
+        process_single_video,
+        img_type=img_type,
+        save_dir=save_dir,
     )
-    class_weights = list(class_weights)
-    class_weights = [round(x, 3) for x in class_weights]
-    logger.info('')
-    logging.info('Class weights: {}'.format(class_weights))
-    class_weights_path = os.path.join(save_dir, 'class_weights.txt')
-    with open(class_weights_path, 'w') as f:
-        f.write(str(class_names) + '\n')
-        f.write(str(tuple(class_weights)))
+    # TODO: change to -1 after debugging
+    result = Parallel(n_jobs=1)(
+        delayed(processing_func)(row) for row in tqdm(df.iterrows(), desc='Dataset conversion', unit=' video')
+    )
 
 
 if __name__ == '__main__':
 
-    CLASSES = (
-        'Background',
-        'Core',
-        'Ring',
-    )
-
     parser = argparse.ArgumentParser(description='Dataset conversion')
     parser.add_argument('--project_dir', required=True, type=str)
-    parser.add_argument('--annotator', default='ViacheslavDanilov', type=str, choices=['MartinaDL', 'ViacheslavDanilov'])
-    parser.add_argument('--class_names', nargs='+', default=CLASSES, type=str)
-    parser.add_argument('--exclude_empty_masks', action='store_true')
-    parser.add_argument('--use_smoothing', action='store_true')
-    parser.add_argument('--img_type',  default='absorbance', type=str, choices=['absorbance', 'hsv', 'reflectance'])
     parser.add_argument('--include_dirs', nargs='+', default=None, type=str)
     parser.add_argument('--exclude_dirs', nargs='+', default=None, type=str)
+    parser.add_argument('--annotator', default='ViacheslavDanilov', type=str, choices=['MartinaDL', 'ViacheslavDanilov'])
+    parser.add_argument('--box_extension', default=10, type=int)
+    parser.add_argument('--img_type',  default='absorbance', type=str, choices=['absorbance', 'hsv'])
     parser.add_argument('--save_dir', required=True, type=str)
     args = parser.parse_args()
 
@@ -204,9 +133,8 @@ if __name__ == '__main__':
 
     main(
         df=df,
-        class_names=tuple(args.class_names),
-        exclude_empty_masks=args.exclude_empty_masks,
-        use_smoothing=args.use_smoothing,
+        annotator=args.annotator,
+        box_extension=args.box_extension,
         img_type=args.img_type,
         save_dir=args.save_dir,
     )
