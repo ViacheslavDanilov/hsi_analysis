733f48197f67364c3327eff9d199973301f9fd94
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2022-09-26 11:32:36 +0300

    Conversion of the Supervisely video dataset (#3)
    * [Fix]: add supervisely library
    
    * [Fix]: exclude temporal files
    
    * [WiP]: add supervisely utility functions
    
    * [WiP]: initialize dataset converter
    
    * [Fix]: update logger timestamp
    
    * [Fix]: adapt read_sly_project to video processing
    
    * [WiP]: add video processing and remove data split
    
    * [Fix]: sort video files
    
    * [Fix]: move crop_image to utility functions
    
    * [WiP]: add multiprocessing parsing of video files
    
    * [Refactor]: change study and series names
    
    * [Feature]: create dataset metadata

---
+++ tools/convert_sly_dataset.py
@@ -0,0 +1,235 @@
+import json
+import argparse
+from typing import Dict
+from functools import partial
+from joblib import Parallel, delayed
+
+from tqdm import tqdm
+
+from tools.utils import crop_image, extract_body_part
+from tools.supervisely_utils import *
+
+
+os.makedirs('logs', exist_ok=True)
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%d.%m.%Y %H:%M:%S',
+    filename='logs/{:s}.log'.format(Path(__file__).stem),
+    filemode='w',
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
+
+
+def get_class_id(
+        class_label: str,
+) -> int:
+
+    if class_label == 'Zone':
+        class_id = 1
+    elif class_label == 'Artifact':
+        class_id = 2
+    else:
+        raise ValueError(f'Unknown class_name: {class_label}')
+    return class_id
+
+
+def get_object_map(
+        objects: List[Dict],
+) -> dict:
+
+    object_map = {}
+    for obj in objects:
+        object_map[obj['key']] = obj['classTitle']
+
+    return object_map
+
+
+def process_single_video(
+        group: Tuple[int, pd.DataFrame],
+        img_type: str,
+        save_dir: str,
+) -> pd.DataFrame:
+    _, row = group
+    study = row['study']
+    series = row['series']
+    video_path = row['video_path']
+
+    # Create image and annotation directories
+    img_dir = os.path.join(save_dir, study, series, 'img')
+    ann_dir = os.path.join(save_dir, study, series, 'ann')
+    os.makedirs(img_dir, exist_ok=True)
+    os.makedirs(ann_dir, exist_ok=True)
+
+    # Read video, save images, and empty annotations
+    df_study = pd.DataFrame(
+        columns=[
+            'Study',
+            'Series',
+            'Body part',
+            'Image path',
+            'Ann path',
+            'Wavelength',
+            'Wavelength ID',
+            'Class label',
+            'Class ID',
+            'x1',
+            'y1',
+            'x2',
+            'y2',
+            'xc',
+            'yc',
+            'Box width',
+            'Box height',
+            'Box area',
+            'Image width',
+            'Image height',
+        ]
+    )
+    video = cv2.VideoCapture(video_path)
+    idx = 0
+    while True:
+        success, _img = video.read()
+        if not success:
+            break
+        img = crop_image(
+            input_img=_img,
+            img_type=img_type
+        )
+        img_path = os.path.join(img_dir, f'{idx+1:03d}.png')
+        img_path_rel = os.path.relpath(img_path, start=save_dir)
+        cv2.imwrite(img_path, img)
+
+        ann_path = os.path.join(ann_dir, f'{idx+1:03d}.txt')
+        ann_path_rel = os.path.relpath(img_path, start=save_dir)
+        f = open(file=ann_path, mode='w')
+        f.close()
+
+        # Add information to the data frame
+        body_part = extract_body_part(img_path_rel)
+        df_study.at[idx, 'Study'] = study
+        df_study.at[idx, 'Series'] = series
+        df_study.at[idx, 'Body part'] = body_part
+        df_study.at[idx, 'Image path'] = img_path_rel
+        df_study.at[idx, 'Ann path'] = ann_path_rel
+        df_study.at[idx, 'Wavelength'] = 500 + 5*idx
+        df_study.at[idx, 'Wavelength ID'] = idx+1
+        df_study.at[idx, 'Image width'] = img.shape[1]
+        df_study.at[idx, 'Image height'] = img.shape[0]
+
+        idx += 1
+
+    # Read json with metadata
+    ann_path = row['ann_path']
+    f = open(ann_path)
+    ann_data = json.load(f)
+    img_height = ann_data['size']['height']
+    img_width = ann_data['size']['width'] // 2          # delimiter is a number of images in a stacked row
+
+    # Extract object keys
+    object_map = get_object_map(objects=ann_data['objects'])
+
+    # Save annotations
+    for frame in ann_data['frames']:
+        frame_idx = frame['index']
+        ann_path = os.path.join(ann_dir, f'{frame_idx + 1:03d}.txt')
+        f = open(file=ann_path, mode='w')
+        for _figure in frame['figures']:
+            figure = sly.Rectangle.from_json(_figure['geometry'])
+            object_key = _figure['objectKey']
+            class_label = object_map[object_key]
+            class_id = get_class_id(class_label)
+
+            # Move box if it was placed on the central or right image
+            if figure.left > img_width:
+                figure = figure.translate(drow=img_width, dcol=0)
+
+            box_info = '{:s} {:d} {:d} {:d} {:d} {:d} {:d} {:d} {:d} {:d} {:d}\n'.format(
+                class_label,                # label of the class e.g. Zone
+                class_id,                   # id number of the class e.g. 1
+                figure.left,                # x1
+                figure.top,                 # y1
+                figure.right,               # x2
+                figure.bottom,              # y2
+                figure.center.col,          # x of a center point
+                figure.center.row,          # y of a center point
+                figure.width,               # box width
+                figure.height,              # box height
+                int(figure.area),           # area of the box
+            )
+            f.write(box_info)
+
+            df_study.at[frame_idx, 'Class label'] = class_label
+            df_study.at[frame_idx, 'Class ID'] = class_id
+            df_study.at[frame_idx, 'x1'] = figure.left
+            df_study.at[frame_idx, 'y1'] = figure.top
+            df_study.at[frame_idx, 'x2'] = figure.right
+            df_study.at[frame_idx, 'y2'] = figure.bottom
+            df_study.at[frame_idx, 'xc'] = figure.center.col
+            df_study.at[frame_idx, 'yc'] = figure.center.row
+            df_study.at[frame_idx, 'Box width'] = figure.height
+            df_study.at[frame_idx, 'Box height'] = figure.height
+            df_study.at[frame_idx, 'Box area'] = int(figure.area)
+
+        f.close()
+
+    return df_study
+
+
+def main(
+        df: pd.DataFrame,
+        box_extension: int,
+        img_type: str,
+        save_dir: str,
+) -> None:
+
+    logger.info(f'Output directory.....: {save_dir}')
+    logger.info(f'Box extension........: {box_extension}')
+
+    study_list = list(set(df['study']))
+    logger.info(f'Number of studies....: {len(study_list)}')
+
+    processing_func = partial(
+        process_single_video,
+        img_type=img_type,
+        save_dir=save_dir,
+    )
+    result = Parallel(n_jobs=-1)(
+        delayed(processing_func)(row) for row in tqdm(df.iterrows(), desc='Dataset conversion', unit=' video')
+    )
+    df_out = pd.concat(result)
+    df_out.reset_index(drop=True, inplace=True)
+    df_out.sort_values(['Study', 'Series'], inplace=True)
+    save_path = os.path.join(save_dir, f'metadata.csv')
+    df_out.to_csv(
+        save_path,
+        index=False,
+    )
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Dataset conversion')
+    parser.add_argument('--project_dir', required=True, type=str)
+    parser.add_argument('--include_dirs', nargs='+', default=None, type=str)
+    parser.add_argument('--exclude_dirs', nargs='+', default=None, type=str)
+    parser.add_argument('--box_extension', default=10, type=int)                # TODO: implement box extension
+    parser.add_argument('--img_type',  default='absorbance', type=str, choices=['absorbance', 'hsv'])
+    parser.add_argument('--save_dir', required=True, type=str)
+    args = parser.parse_args()
+
+    df = read_sly_project(
+        project_dir=args.project_dir,
+        include_dirs=args.include_dirs,
+        exclude_dirs=args.exclude_dirs,
+    )
+
+    main(
+        df=df,
+        box_extension=args.box_extension,
+        img_type=args.img_type,
+        save_dir=args.save_dir,
+    )
+
+    logger.info('')
+    logger.info('Dataset conversion complete')
---
+++ tools/supervisely_utils.py
@@ -0,0 +1,145 @@
+import io
+import os
+import zlib
+import base64
+import logging
+from PIL import Image
+from pathlib import Path
+from typing import List, Tuple, Optional
+
+import cv2
+import numpy as np
+import pandas as pd
+import supervisely_lib as sly
+from scipy.ndimage import binary_opening, binary_fill_holes
+
+
+# TODO: remove it in case of usage object detection approach
+def get_class_color(
+        class_name: str,
+) -> List[int]:
+
+    try:
+        mapping_dict = {
+            'Background': [128, 128, 128],
+            'Core': [105, 45, 33],
+            'Ring': [196, 156, 148],
+        }
+        return mapping_dict[class_name]
+    except Exception as e:
+        raise ValueError('Unrecognized class_name: {:s}'.format(class_name))
+
+
+def get_palette(
+        class_names: Tuple[str],
+) -> List[List[int]]:
+
+    palette = []
+    for class_name in class_names:
+        class_color = get_class_color(class_name)
+        palette.append(class_color)
+
+    return palette
+
+
+def read_sly_project(
+    project_dir: str,
+    include_dirs: Optional[List[str]] = None,
+    exclude_dirs: Optional[List[str]] = None,
+) -> pd.DataFrame:
+
+    logging.info(f'Dataset dir..........: {project_dir}')
+    assert os.path.exists(project_dir) and os.path.isdir(project_dir), 'Wrong project dir: {}'.format(project_dir)
+    project = sly.VideoProject(
+        directory=project_dir,
+        mode=sly.OpenMode.READ,
+    )
+
+    series_list: List[str] = []
+    study_list: List[str] = []
+    video_paths: List[str] = []
+    ann_paths: List[str] = []
+
+    for dataset in project:
+        study = dataset.name
+
+        if include_dirs and study not in include_dirs:
+            logging.info(f'Excluded dir.........: {study}')
+            continue
+
+        if exclude_dirs and study in exclude_dirs:
+            logging.info(f'Excluded dir.........: {study}')
+            continue
+
+        logging.info(f'Included dir.........: {study}')
+        for item_name in dataset:
+            video_path, ann_path = dataset.get_item_paths(item_name)
+            series = Path(video_path).stem
+            series_list.append(series)
+            video_paths.append(video_path)
+            ann_paths.append(ann_path)
+            study_list.append(study)
+
+    df = pd.DataFrame.from_dict(
+        {
+            'study': study_list,
+            'series': series_list,
+            'video_path': video_paths,
+            'ann_path': ann_paths,
+        }
+    )
+    df.sort_values(['study', 'series'], inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    return df
+
+
+def mask_to_base64(mask: np.array):
+    img_pil = Image.fromarray(np.array(mask, dtype=np.uint8))
+    img_pil.putpalette([0, 0, 0, 255, 255, 255])
+    bytes_io = io.BytesIO()
+    img_pil.save(bytes_io, format="PNG", transparency=0, optimize=0)
+    bytes = bytes_io.getvalue()
+    return base64.b64encode(zlib.compress(bytes)).decode("utf-8")
+
+
+def base64_to_mask(s: str) -> np.ndarray:
+    z = zlib.decompress(base64.b64decode(s))
+    n = np.frombuffer(z, np.uint8)
+    img_decoded = cv2.imdecode(n, cv2.IMREAD_UNCHANGED)
+    if (len(img_decoded.shape) == 3) and (img_decoded.shape[2] >= 4):
+        mask = img_decoded[:, :, 3].astype(np.uint8)        # 4-channel images
+    elif len(img_decoded.shape) == 2:
+        mask = img_decoded.astype(np.uint8)                 # 1-channel images
+    else:
+        raise RuntimeError("Wrong internal mask format.")
+    return mask
+
+
+def smooth_mask(
+        binary_mask: np.ndarray,
+) -> np.ndarray:
+    # kernel = cv2.getStructuringElement(shape=cv2.MORPH_ELLIPSE, ksize=(5, 5))
+    # binary_mask = binary_fill_holes(binary_mask, structure=None)                      # FIXME: fills big holes
+    binary_mask = binary_opening(binary_mask, structure=None)
+    binary_mask = 255 * binary_mask.astype(np.uint8)
+    return binary_mask
+
+
+def insert_mask(
+        mask: np.ndarray,
+        obj_mask: np.ndarray,
+        origin: List[int],
+) -> np.ndarray:
+
+    x, y = origin
+    obj_mask_height = obj_mask.shape[0]
+    obj_mask_width = obj_mask.shape[1]
+
+    for idx_y in range(obj_mask_height):
+        for idx_x in range(obj_mask_width):
+            pixel_value = obj_mask[idx_y, idx_x]
+            # Check if it is a zero-intensity pixel
+            if np.sum(pixel_value) != 0:
+                mask[idx_y + y, idx_x + x] = pixel_value
+
+    return mask
