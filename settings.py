4995745855f7cd5d6e928e465e519bd8844e1e57
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2022-12-15 17:46:03 +0100

    [Feature]: add a file with default parameter values

---
+++ settings.py
@@ -0,0 +1,11 @@
+# Supervisely to COCO
+SLY_PROJECT_DIR = 'dataset/SLY'
+COCO_SAVE_DIR = 'dataset/COCO'
+INCLUDE_DIRS = []
+EXCLUDE_DIRS = []
+TRAIN_SIZE = 0.80
+SEED = 11
+BOX_EXTENSION = {
+    # class: (horizontal, vertical)
+    'Ablation': (0, 0),
+}
