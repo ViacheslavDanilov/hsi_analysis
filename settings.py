f36ee55fa27572b86c1484f9025a52464dcd1fef
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2022-12-15 20:06:56 +0100

    [Fix]: exclude problem studies

--- settings.py
+++ settings.py
@@ -2,7 +2,12 @@
 SLY_PROJECT_DIR = 'dataset/SLY'
 COCO_SAVE_DIR = 'dataset/COCO'
 INCLUDE_DIRS = []
-EXCLUDE_DIRS = []
+EXCLUDE_DIRS = [
+    '29_08_2019_test_08_pancreas',
+    '30_08_2019_test_04_pancreas',
+    '30_08_2019_test_06_pancreas',
+    '30_08_2019_test_08_stomach',
+]
 TRAIN_SIZE = 0.80
 SEED = 11
 BOX_EXTENSION = {
