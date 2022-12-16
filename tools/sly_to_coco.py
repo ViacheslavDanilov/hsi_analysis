fa6881e7c5ec39cda2b0fc8c69f63fc844c25a14
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2022-12-16 16:56:08 +0100

    [WiP]: implement dataset conversion

--- tools/sly_to_coco.py
+++ tools/sly_to_coco.py
@@ -1,15 +1,15 @@
 import os
-import json
 import logging
 import argparse
 from pathlib import Path
 
-import cv2
 import pandas as pd
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 
-from tools.supervisely_utils import read_sly_project
+from tools.utils import copy_files
+from tools.utils_coco import get_img_info, get_ann_info
+from tools.utils_sly import CLASS_MAP, read_sly_project
 from settings import (
     SLY_PROJECT_DIR,
     INCLUDE_DIRS,
@@ -34,6 +34,14 @@ logger = logging.getLogger(__name__)
 def add_metadata(
     df: pd.DataFrame,
 ) -> pd.DataFrame:
+    """
+
+    Args:
+        df: a source data frame with dataset metadata
+
+    Returns:
+        df: an updated data frame with split series into series names and PCs
+    """
 
     df['PC'] = df['series'].apply(lambda x: int(x.split('_')[-1]))
     df['series'] = df['series'].apply(lambda x: '_'.join(x.split('_')[:-1]))
@@ -46,7 +54,16 @@ def prepare_subsets(
     train_size: float = 0.80,
     seed: int = 11,
 ) -> dict:
+    """
 
+    Args:
+        df: data frame derived from a metadata file
+        train_size: a fraction used to split dataset into train and test subsets
+        seed: random value for splitting train and test subsets
+
+    Returns:
+        subsets: dictionary which contains image/annotation paths for train and test subsets
+    """
     subsets = {
         'train': {'images': [], 'labels': []},
         'test': {'images': [], 'labels': []},
@@ -84,9 +101,55 @@ def prepare_coco(
     box_extension: dict,
     save_dir: str,
 ) -> None:
+    """
 
-    # TODO: add support for COCO dataset parsing
-    print('Complete')
+    Args:
+        subsets: dictionary which contains image/annotation paths for train and test subsets
+        save_dir: directory where split datasets are saved to
+        box_extension: a value used to extend or contract object box sizes
+
+    Returns:
+        None
+    """
+
+    categories_coco = []
+    for idx, (key, value) in enumerate(CLASS_MAP.items()):
+        categories_coco.append({'id': value, 'name': key})
+
+    for subset_name, subset in subsets.items():
+        imgs_coco = []
+        anns_coco = []
+        ann_id = 0
+        for img_id, (img_path, ann_path) in tqdm(
+            enumerate(zip(subset['images'], subset['labels'])),
+            desc=f'{subset_name.capitalize()} subset processing',
+            unit=' sample',
+        ):
+            img_data = get_img_info(
+                img_path=img_path,
+                img_id=img_id,
+            )
+            # TODO: add support for COCO dataset parsing
+            ann_data, ann_id = get_ann_info(
+                label_path=ann_path,
+                img_id=img_id,
+                ann_id=ann_id,
+                box_extension=box_extension,
+            )
+            imgs_coco.append(img_data)
+            anns_coco.extend(ann_data)
+
+        dataset = {
+            'images': imgs_coco,
+            'annotations': anns_coco,
+            'categories': categories_coco,
+        }
+    # TODO: verify saved files
+    # save_img_dir = os.path.join(save_dir, subset_name, 'data')
+    # copy_files(file_list=subset['images'], save_dir=save_img_dir)
+    # save_ann_path = os.path.join(save_dir, subset_name, 'labels.json')
+    # with open(save_ann_path, 'w') as file:
+    #     json.dump(dataset, file)
 
 
 def main(
