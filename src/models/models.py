ae7ac08fa5f479105928091f172cfca6363d8253
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2023-03-18 20:55:59 +0700

    [Feature]: convert detections to a dataframe

--- src/models/models.py
+++ src/models/models.py
@@ -1,6 +1,8 @@
 import logging
+from pathlib import Path
 from typing import List
 
+import numpy as np
 import pandas as pd
 import torch
 from cpuinfo import get_cpu_info
@@ -10,13 +12,12 @@ from src.data.utils import get_file_list
 
 
 class AblationDetector:
-    """A class used during the inference of the ablation detection pipeline."""
+    """A class used during the inference of the detection pipeline."""
 
     def __init__(
         self,
         model_dir: str,
-        conf_threshold: float = 0.5,  # TODO: implement usage of this parameter
-        iou_threshold: float = 0.3,  # TODO: implement usage of this parameter
+        conf_threshold: float = 0.01,
         device: str = 'auto',
     ):
 
@@ -51,6 +52,8 @@ class AblationDetector:
             checkpoint=checkpoint_path,
             device=device_,
         )
+        self.classes = self.model.CLASSES
+        self.model.test_cfg.rcnn.score_thr = conf_threshold
 
         # Log the device that is used for the prediction
         logging.info(f'Device......:')
@@ -62,15 +65,64 @@ class AblationDetector:
             info = get_cpu_info()
             logging.info(f'CPU.........: {info["brand_raw"]}')
 
-    # TODO: add method for forecasting
     def predict(
         self,
         img_paths: List[str],
-    ) -> pd.DataFrame:
-        # output -> array(x_min, y_min, x_max, y_max, confidence)
+    ) -> List[List[np.ndarray]]:
         detections = inference_detector(
             model=self.model,
             imgs=img_paths,
         )
 
-        return detections  # FIXME: change type
+        return detections
+
+    def process_detections(
+        self,
+        img_paths: List[str],
+        detections: List[List[np.ndarray]],
+    ) -> pd.DataFrame:
+
+        columns = [
+            'img_path',
+            'img_name',
+            'img_height',
+            'img_width',
+            'x1',
+            'y1',
+            'x2',
+            'y2',
+            'class_id',
+            'class',
+            'confidence',
+        ]
+
+        # Iterate over images
+        df = pd.DataFrame(columns=columns)
+        for image_idx, (img_path, detections_image) in enumerate(zip(img_paths, detections)):
+
+            # Iterate over class detections
+            for class_idx, detections_class in enumerate(detections_image):
+                if detections_class.size == 0:
+                    num_detections = 1
+                else:
+                    num_detections = detections_class.shape[0]
+
+                # Iterate over boxes on a single image
+                df_ = pd.DataFrame(index=range(num_detections), columns=columns)
+                df_['img_path'] = img_path
+                df_['img_name'] = Path(img_path).name
+                for idx, box in enumerate(detections_class):
+                    # box -> array(x_min, y_min, x_max, y_max, confidence)
+                    df_.at[idx, 'x1'] = int(box[0])
+                    df_.at[idx, 'y1'] = int(box[1])
+                    df_.at[idx, 'x2'] = int(box[2])
+                    df_.at[idx, 'y2'] = int(box[3])
+                    df_.at[idx, 'class_id'] = class_idx
+                    df_.at[idx, 'class'] = self.classes[class_idx]
+                    df_.at[idx, 'confidence'] = box[4]
+                df = pd.concat([df, df_])
+
+        df.sort_values('img_path', inplace=True)
+        df.reset_index(drop=True, inplace=True)
+
+        return df
